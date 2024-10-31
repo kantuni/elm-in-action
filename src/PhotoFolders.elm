042687bf397119c09ba17fb3fb12194b03dc16ca
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-10-31 13:22:41 -0400

    7.1.2

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -1,26 +1,37 @@
 module PhotoFolders exposing (main)
 
 import Browser
-import Html exposing (Html, h1, text)
+import Dict exposing (Dict)
+import Html exposing (Html, div, h1, h2, h3, img, span, text)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 import Http
 import Json.Decode exposing (Decoder)
 
 
+urlPrefix : String
+urlPrefix =
+    "https://elm-in-action.com/"
+
+
 type alias Model =
     { selectedPhotoUrl : Maybe String
+    , photos : Dict String Photo
     }
 
 
 initialModel : Model
 initialModel =
-    { selectedPhotoUrl = Nothing }
+    { selectedPhotoUrl = Nothing
+    , photos = Dict.empty
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( initialModel
     , Http.get
-        { url = "https:://elm-in-action.com/folders/list"
+        { url = urlPrefix ++ "folders/list"
         , expect = Http.expectJson GotInitialModel modelDecoder
         }
     )
@@ -28,7 +39,33 @@ init _ =
 
 modelDecoder : Decoder Model
 modelDecoder =
-    Json.Decode.succeed initialModel
+    Json.Decode.succeed
+        { selectedPhotoUrl = Just "trevi"
+        , photos =
+            Dict.fromList
+                [ ( "trevi"
+                  , { title = "Trevi"
+                    , relatedUrls = [ "coli", "fresco" ]
+                    , size = 34
+                    , url = "trevi"
+                    }
+                  )
+                , ( "fresco"
+                  , { title = "Fresco"
+                    , relatedUrls = [ "trevi" ]
+                    , size = 46
+                    , url = "fresco"
+                    }
+                  )
+                , ( "coli"
+                  , { title = "Coliseum"
+                    , relatedUrls = [ "trevi", "fresco" ]
+                    , size = 36
+                    , url = "coli"
+                    }
+                  )
+                ]
+        }
 
 
 type Msg
@@ -49,9 +86,54 @@ update msg model =
             ( model, Cmd.none )
 
 
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "The Grooviest Folders the world has ever seen" ]
+    let
+        photoByUrl : String -> Maybe Photo
+        photoByUrl url =
+            Dict.get url model.photos
+
+        selectedPhoto : Html Msg
+        selectedPhoto =
+            case Maybe.andThen photoByUrl model.selectedPhotoUrl of
+                Just photo ->
+                    viewSelectedPhoto photo
+
+                Nothing ->
+                    text ""
+    in
+    div [ class "content" ]
+        [ selectedPhoto ]
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src (urlPrefix ++ "photos/" ++ photo.url ++ "/full") ] []
+        , span [] [ text (String.fromInt photo.size ++ "KB") ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ]
+            (List.map viewRelatedPhoto photo.relatedUrls)
+        ]
+
+
+viewRelatedPhoto : String -> Html Msg
+viewRelatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (ClickedPhoto url)
+        , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
+        ]
+        []
 
 
 main : Program () Model Msg
