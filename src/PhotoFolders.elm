78d39128d439d26f8088e3fe2a8fbc6a26b9e94c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-11-04 17:00:38 -0500

    7.2

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -1,12 +1,18 @@
 module PhotoFolders exposing (main)
 
+-- TODO: Clicking a related photo should expand the folder path to that photo.
+
 import Browser
 import Dict exposing (Dict)
-import Html exposing (Html, div, h1, h2, h3, img, span, text)
+import Html exposing (Html, div, h1, h2, h3, img, label, span, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode exposing (Decoder)
+
+
+
+-- INIT
 
 
 urlPrefix : String
@@ -14,9 +20,27 @@ urlPrefix =
     "https://elm-in-action.com/"
 
 
+type Folder
+    = Folder
+        { name : String
+        , expanded : Bool
+        , photoUrls : List String
+        , subfolders : List Folder
+        }
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
 type alias Model =
     { selectedPhotoUrl : Maybe String
     , photos : Dict String Photo
+    , root : Folder
     }
 
 
@@ -24,6 +48,13 @@ initialModel : Model
 initialModel =
     { selectedPhotoUrl = Nothing
     , photos = Dict.empty
+    , root =
+        Folder
+            { name = "Loading..."
+            , expanded = True
+            , photoUrls = []
+            , subfolders = []
+            }
     }
 
 
@@ -65,33 +96,111 @@ modelDecoder =
                     }
                   )
                 ]
+        , root =
+            Folder
+                { name = "Photos"
+                , expanded = True
+                , photoUrls = []
+                , subfolders =
+                    [ Folder
+                        { name = "2016"
+                        , expanded = True
+                        , photoUrls = [ "trevi", "coli" ]
+                        , subfolders =
+                            [ Folder
+                                { name = "outdoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            , Folder
+                                { name = "indoors"
+                                , expanded = True
+                                , photoUrls = [ "fresco" ]
+                                , subfolders = []
+                                }
+                            ]
+                        }
+                    , Folder
+                        { name = "2017"
+                        , expanded = True
+                        , photoUrls = []
+                        , subfolders =
+                            [ Folder
+                                { name = "outdoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            , Folder
+                                { name = "indoors"
+                                , expanded = True
+                                , photoUrls = []
+                                , subfolders = []
+                                }
+                            ]
+                        }
+                    ]
+                }
         }
 
 
+
+-- UPDATE
+
+
 type Msg
-    = ClickedPhoto String
-    | GotInitialModel (Result Http.Error Model)
+    = GotInitialModel (Result Http.Error Model)
+    | ClickedPhoto String
+    | ClickedFolder FolderPath
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ClickedPhoto url ->
-            ( { model | selectedPhotoUrl = Just url }, Cmd.none )
-
         GotInitialModel (Ok newModel) ->
             ( newModel, Cmd.none )
 
         GotInitialModel (Err _) ->
             ( model, Cmd.none )
 
+        ClickedPhoto url ->
+            ( { model | selectedPhotoUrl = Just url }, Cmd.none )
 
-type alias Photo =
-    { title : String
-    , size : Int
-    , relatedUrls : List String
-    , url : String
-    }
+        ClickedFolder path ->
+            ( { model | root = toggleExpanded path model.root }, Cmd.none )
+
+
+type FolderPath
+    = End
+    | Subfolder Int FolderPath
+
+
+toggleExpanded : FolderPath -> Folder -> Folder
+toggleExpanded path (Folder folder) =
+    case path of
+        End ->
+            Folder { folder | expanded = not folder.expanded }
+
+        Subfolder targetIndex remainingPath ->
+            let
+                subfolders : List Folder
+                subfolders =
+                    List.indexedMap transform folder.subfolders
+
+                transform : Int -> Folder -> Folder
+                transform currentIndex currentSubfolder =
+                    if currentIndex == targetIndex then
+                        toggleExpanded remainingPath currentSubfolder
+
+                    else
+                        currentSubfolder
+            in
+            Folder { folder | subfolders = subfolders }
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
@@ -111,7 +220,18 @@ view model =
                     text ""
     in
     div [ class "content" ]
-        [ selectedPhoto ]
+        [ div [ class "folders" ]
+            [ h1 [] [ text "Folders" ]
+            , viewFolder End model.root
+            ]
+        , selectedPhoto
+        ]
+
+
+viewPhoto : String -> Html Msg
+viewPhoto url =
+    div [ class "photo", onClick (ClickedPhoto url) ]
+        [ text url ]
 
 
 viewSelectedPhoto : Photo -> Html Msg
@@ -134,6 +254,46 @@ viewRelatedPhoto url =
         , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
         ]
         []
+
+
+viewFolder : FolderPath -> Folder -> Html Msg
+viewFolder path (Folder folder) =
+    let
+        viewSubfolder : Int -> Folder -> Html Msg
+        viewSubfolder index subfolder =
+            viewFolder (appendIndex index path) subfolder
+
+        folderLabel =
+            label [ onClick (ClickedFolder path) ] [ text folder.name ]
+    in
+    if folder.expanded then
+        let
+            contents =
+                List.append
+                    (List.indexedMap viewSubfolder folder.subfolders)
+                    (List.map viewPhoto folder.photoUrls)
+        in
+        div [ class "folder expanded" ]
+            [ folderLabel
+            , div [ class "contents" ] contents
+            ]
+
+    else
+        div [ class "folder collapsed" ] [ folderLabel ]
+
+
+appendIndex : Int -> FolderPath -> FolderPath
+appendIndex index path =
+    case path of
+        End ->
+            Subfolder index End
+
+        Subfolder subfolderIndex remainingPath ->
+            Subfolder subfolderIndex (appendIndex index remainingPath)
+
+
+
+-- MAIN
 
 
 main : Program () Model Msg
