0ce3d9c0e7a9571c1cfe73cff37e407ba50ec13a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-11-11 05:29:00 -0500

    8.2

---
+++ src/Main.elm
@@ -0,0 +1,173 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Browser.Navigation as Nav
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s)
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { key : Nav.Key
+    , page : Page
+    }
+
+
+type Page
+    = Folders
+    | Gallery
+    | SelectedPhoto String
+    | NotFound
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( { key = key
+      , page = urlToPage url
+      }
+    , Cmd.none
+    )
+
+
+urlToPage : Url -> Page
+urlToPage url =
+    Parser.parse parser url
+        |> Maybe.withDefault NotFound
+
+
+parser : Parser (Page -> a) a
+parser =
+    Parser.oneOf
+        [ Parser.map Folders Parser.top
+        , Parser.map Gallery (s "gallery")
+        , Parser.map SelectedPhoto (s "photos" </> Parser.string)
+        ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ClickedLink Browser.UrlRequest
+    | ChangedUrl Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedLink urlRequest ->
+            case urlRequest of
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+        ChangedUrl url ->
+            ( { model | page = urlToPage url }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            text "This isn't even my final form!"
+    in
+    { title = "Photo Groove, SPA Style"
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li
+                [ classList
+                    [ ( "active", isActive { link = targetPage, page = page } )
+                    ]
+                ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+isActive : { link : Page, page : Page } -> Bool
+isActive { link, page } =
+    case ( link, page ) of
+        ( Gallery, Gallery ) ->
+            True
+
+        ( Gallery, _ ) ->
+            False
+
+        ( Folders, Folders ) ->
+            True
+
+        ( Folders, SelectedPhoto _ ) ->
+            True
+
+        ( Folders, _ ) ->
+            False
+
+        ( SelectedPhoto _, _ ) ->
+            False
+
+        ( NotFound, _ ) ->
+            False
+
+
+viewFooter : Html msg
+viewFooter =
+    footer []
+        [ text "One is never alone with a rubber duck. – Douglas Adams" ]
